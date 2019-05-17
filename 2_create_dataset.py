edcd5b2cbb121ae8ef8c9dc8ea9a18179a6f7cb8
Py CP <bijoycp90@gmail.com>
2019-05-17 16:00:44 +0530

    Update 2_create_dataset.py

--- 2_create_dataset.py
+++ 2_create_dataset.py
@@ -10,19 +10,23 @@ IMG_SIZE = 96
 def create_train_data():
     training_data = []
     label=0
-    for (dirpath,dirnames,filenames) in os.walk(path,topdown=True):
-        print(dirpath)
-        print(dirnames)
-        # print(filenames)
+    for (dirpath,dirnames,filenames) in os.walk(path):
         for dirname in dirnames:
-            for(direcpath,direcnames,files) in os.walk(path+"/"+dirname,topdown=True):
+            print(dirname)
+            for(direcpath,direcnames,files) in os.walk(path+"/"+dirname):
                 for file in files:
-                    actual_path=path+"/"+dirname+"/"+file
-                    # print(actual_path)
-                    img=cv2.imread(actual_path,cv2.IMREAD_GRAYSCALE)                     
-                    print(img)
-                    
-
-                    
+                        actual_path=path+"/"+dirname+"/"+file
+                        print(files)
+                        # label=label_img(dirname)
+                        path1 =path+"/"+dirname+'/'+file
+                        img=cv2.imread(path1,cv2.IMREAD_GRAYSCALE)
+                        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+                        training_data.append([np.array(img),label])
+            label=label+1
+            print(label)
+    shuffle(training_data)
+    np.save('train_data.npy', training_data)
+    print(training_data)
+    return training_data
 
 create_train_data()
